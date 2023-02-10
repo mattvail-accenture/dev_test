7b8bfb379a84a8e488ec173a7d3586331fe08417
mattvail-accenture <113193795+mattvail-accenture@users.noreply.github.com>
2023-02-10 20:27:33 +0000

    upload the test

---
+++ main.py
@@ -0,0 +1,61 @@
+tvShows = [
+    {
+        "id": "1234",
+        "Episode_Name": "Over the rainbow",
+        "tvShowName": "Bleach",
+        "Producer": "Funimation",
+        "producer_Address": "1982 walwala Cali",
+        "voice_actor": [
+            {"Name": "Lee Jen",
+             "charactor Name": "Ichigo"},
+            {"Name": "howw VIL",
+             "charactor Name": "Rukia"},
+            {"Name": "DD Bon",
+             "charactor Name": "Chad",
+             "age": 21},
+        ],
+        "watchTime": 1223
+    },
+    {
+        "id": "998878",
+        "Episode_Name": "Under the rainbow",
+        "tvShowName": "Bleach",
+        "Producer": "Funimation",
+        "producer_Address": "1982 walwala Cali",
+        "voice_actor": [
+            {"Name": "Lee Jen",
+             "charactor Name": "Ichigo"},
+        ],
+        "watchTime": 3833
+    },
+    {
+        "id": "1938",
+        "Episode_Name": "Over the rainbow",
+        "tvShowName": "Dragon Ball Z",
+        "Producer": "CrunchyRoll",
+        "producer_Address": "8872 walwala Cali",
+        "voice_actor": [
+            {"Name": "howw VIL",
+             "charactor Name": "Chi Chi"},
+            {"Name": "DD Bon",
+             "charactor Name": "GOku",
+             "age": 21},
+        ],
+        "watchTime": 1223
+    },
+    {
+        "id": "123412334",
+        "Episode_Name": "Over the rainbow",
+        "tvShowName": "Salior Moon",
+        "Producer": "Cartoon Network",
+        "producer_Address": "123 edmon TX",
+        "voice_actor": [
+            {"Name": "DD Bon",
+             "charactor Name": "The mask",
+             "age": 21},
+        ],
+        "watchTime": 1223
+    },
+]
+
+
